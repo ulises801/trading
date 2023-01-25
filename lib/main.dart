16ce790506fd224aa4dc0438f9bdc7f90423cbd4
ulises801 <ulises-801@outlook.es>
2023-01-25 05:17:39 -0600

    Achievements section

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,8 @@ class MyHomePage extends StatelessWidget {
               arriba(),
               SizedBox(height: 20),
               userBalance(),
+              sectionTitle(),
+              achievements()
             ]),
       ),
     );
@@ -148,6 +150,163 @@ class userBalance extends StatelessWidget {
             ),
           ),
         ),
+      ],
+    );
+  }
+}
+
+class sectionTitle extends StatelessWidget {
+  const sectionTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 50, bottom: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Achievements',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'See all',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: MyApp.orange),
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class achievements extends StatelessWidget {
+  const achievements({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 100,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color(0xFFFF9346)),
+                margin: EdgeInsets.all(4),
+                width: 100.0,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '💣',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '1 week streak',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.w500),
+                      )
+                    ]),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color(0xFFBFEAB9)),
+                margin: EdgeInsets.all(4),
+                width: 100.0,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '🐉',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '3 week streak',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.w500),
+                      )
+                    ]),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color(0xFFADF1FB)),
+                margin: EdgeInsets.all(4),
+                width: 100.0,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '💪',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '7 week streak',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.w500),
+                      )
+                    ]),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color(0xFFFFC2DF)),
+                margin: EdgeInsets.all(4),
+                width: 100.0,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '🚀',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '12 week streak',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.w500),
+                      )
+                    ]),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                    color: Color.fromARGB(255, 247, 52, 52)),
+                margin: EdgeInsets.all(4),
+                width: 100.0,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        '🔱',
+                        style: TextStyle(fontSize: 35),
+                      ),
+                      Text(
+                        '20 week streak',
+                        style: TextStyle(
+                            fontSize: 11, fontWeight: FontWeight.w500),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
