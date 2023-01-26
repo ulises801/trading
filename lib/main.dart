ea308809e7b17d6e2a1b362eb9a9831522065edd
ulises801 <ulises-801@outlook.es>
2023-01-26 00:10:06 -0600

    Pantalla profile, profile photo y contadores de posts, followers y subscriptions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trading/widgets/appBar.dart';
 import 'package:trading/widgets/sideBar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(const MyApp());
 
@@ -47,7 +48,8 @@ class MyHomePage extends StatelessWidget {
               SizedBox(height: 20),
               userBalance(),
               sectionTitle(),
-              achievements()
+              achievements(),
+              sectionTitle2()
             ]),
       ),
     );
@@ -192,6 +194,43 @@ class sectionTitle extends StatelessWidget {
   }
 }
 
+class sectionTitle2 extends StatelessWidget {
+  const sectionTitle2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 50, bottom: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Investment portfolio',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'See all',
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      color: MyApp.orange),
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
+
 class achievements extends StatelessWidget {
   const achievements({super.key});
 
@@ -309,5 +348,175 @@ class achievements extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyApp.black,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: MyApp.black,
+        title: const Text('Profile'),
+      ),
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 60, left: 15, right: 15, bottom: 15),
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/profile2.jpeg')),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 22),
+              child: Column(
+                children: [
+                  Text(
+                    'Victor Hugo',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 6),
+                    child: Text(
+                      '@victur13',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.grey),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: Color(0xFFBFEAB9)),
+                      height: 100,
+                      width: 125,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '32',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            'Posts',
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: MyApp.darkBlue),
+                      height: 100,
+                      width: 125,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '26354',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            'Followers',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          color: MyApp.darkBlue),
+                      height: 100,
+                      width: 125,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '256',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            'Subscriptions',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+            sectionTitle3()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class sectionTitle3 extends StatelessWidget {
+  const sectionTitle3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 50, bottom: 20),
+        padding: EdgeInsets.symmetric(horizontal: 9.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Posts',
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'Sorting',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: MyApp.orange),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/sideBar.dart
+++ lib/widgets/sideBar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 import '../main.dart';
 
 Drawer sideBarPersonalizado(BuildContext context) {
@@ -12,37 +12,30 @@ Drawer sideBarPersonalizado(BuildContext context) {
       // Important: Remove any padding from the ListView.
       padding: EdgeInsets.zero,
       children: [
-        const DrawerHeader(
-          decoration: BoxDecoration(
-            color: MyApp.black,
-          ),
-          child: Text(
-            'Drawer Header',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
+        DrawerHeader(
+            decoration: BoxDecoration(
+              color: MyApp.black,
+            ),
+            child: SvgPicture.asset(
+              'assets/logo.svg',
+            )),
         ListTile(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.white, width: 0.3),
+          ),
           title: const Text(
-            'Item 1',
+            'Profile',
             style: TextStyle(color: Colors.white),
           ),
           onTap: () {
             // Update the state of the app
             // ...
             // Then close the drawer
-            Navigator.pop(context);
-          },
-        ),
-        ListTile(
-          title: const Text(
-            'Item 2',
-            style: TextStyle(color: Colors.white),
-          ),
-          onTap: () {
-            // Update the state of the app
-            // ...
-            // Then close the drawer
-            Navigator.pop(context);
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
           },
         ),
       ],
